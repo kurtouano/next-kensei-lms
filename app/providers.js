dea18f3f88649e5f77df400b4b9fa20b17d2cd50
kurtouano <kurt0216@gmail.com>
2025-08-11 01:11:58 +0800

    Fix: stop rerendering upon leaving page or page unfocus

--- app/providers.js
+++ app/providers.js
@@ -3,5 +3,13 @@
 import { SessionProvider } from "next-auth/react";
 
 export const AuthProvider = ({ children }) => {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider
+      refetchInterval={0} // Disable automatic refetching
+      refetchOnWindowFocus={false} // Disable refetch on window focus
+      refetchWhenOffline={false} // Disable refetch when offline
+    >
+      {children}
+    </SessionProvider>
+  );
 };
