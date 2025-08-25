6f92d37b95927a14870d38894f9b7a24ff133169
kurtouano <kurt0216@gmail.com>
2025-08-26 04:54:39 +0800

    fix: connectdb error in friends status monitoring

--- app/api/friends/stream/route.js
+++ app/api/friends/stream/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
-import connectDb from '@/lib/mongodb';
+import { connectDb } from '@/lib/mongodb';
 import Friend from '@/models/Friend';
 import User from '@/models/User';
 
