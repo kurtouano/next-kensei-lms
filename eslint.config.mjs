3253bdf39aa042af83ae3ffaae78e5ace8b09981
kurtouano <kurt0216@gmail.com>
2025-12-10 18:05:21 +0800

    fix(eslist): - fix eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,5 +5,17 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
+  {
+    ...pluginReact.configs.flat.recommended,
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      ...pluginReact.configs.flat.recommended.rules,
+      "react/react-in-jsx-scope": "off", // Not needed in React 17+ and Next.js
+      "react/no-unescaped-entities": "warn", // Change to warning instead of error
+    },
+  },
 ]);
